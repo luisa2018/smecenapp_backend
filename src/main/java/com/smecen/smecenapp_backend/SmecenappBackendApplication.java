4d0db7b2c7acb72b70f0127f7dd55e4c75c39638
Luisa <luisa.fero@hotmail.com>
2025-04-23 12:06:10 -0500

    Initial commit

---
+++ src/main/java/com/smecen/smecenapp_backend/SmecenappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smecen.smecenapp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmecenappBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmecenappBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smecen/smecenapp_backend/SmecenappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smecen.smecenapp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmecenappBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
