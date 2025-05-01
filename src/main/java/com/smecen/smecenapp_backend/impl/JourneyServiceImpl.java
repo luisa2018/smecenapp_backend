717ef4c8dd430effd044f5dd57ec2a9198eddb4c
Luisa <luisa.fero@hotmail.com>
2025-04-30 22:11:36 -0500

    WIP: cambios en interfaces y servicios Journey

--- src/main/java/com/smecen/smecenapp_backend/impl/JourneyServiceImpl.java
+++ src/main/java/com/smecen/smecenapp_backend/impl/JourneyServiceImpl.java
@@ -1,2 +1,39 @@
-package com.smecen.smecenapp_backend.impl;public class JourneyImpl {
+package com.smecen.smecenapp_backend.impl;
+
+import com.smecen.smecenapp_backend.model.Journey;
+import com.smecen.smecenapp_backend.repository.IJourneyRepo;
+import com.smecen.smecenapp_backend.service.IJourneyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class JourneyServiceImpl  implements IJourneyService {
+
+    @Autowired
+    private IJourneyRepo repo;
+    @Override
+    public Journey registrar(Journey p) throws Exception {
+        return repo.save(p);
+    }
+
+    @Override
+    public Journey modifcar(Journey p) throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<Journey> listar() throws Exception {
+        return null;
+    }
+
+    @Override
+    public Journey listarPorId(Integer integer) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void eliminar(Integer integer) throws Exception {
+
+    }
 }
